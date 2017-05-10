95dcbeeec66fa5a83fab55fe6ec6f6722cc49f5e
Nico Kreipke <git@nkreipke.de>
2017-05-10 11:53:17 +0200

    Signed the assembly

--- src/Sprache/Properties/AssemblyInfo.cs
+++ src/Sprache/Properties/AssemblyInfo.cs
@@ -4,4 +4,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTitle("Sprache Tiny C# Parser Combinators")]
 [assembly: AssemblyProduct("Sprache")]
 [assembly: AssemblyCopyright("Copyright Sprache Contributors")]
+
+#if !SIGN
 [assembly: InternalsVisibleTo("Sprache.Tests")]
+#endif
